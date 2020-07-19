54c69dd0814f5250b6276746acd46e859e118404
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-07-19 20:25:24 +0530

    UI

---
+++ app/src/main/java/com/ddvader44/trackcovid_19/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ddvader44.trackcovid_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void trackYourCountry(View view) {
+
+    }
+}
