41bfd6bb92212fd7d04e53198d31b2f73a407bce
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-07-21 00:22:26 +0530

    MainActivity done

---
+++ app/src/main/java/com/ddvader44/trackcovid_19/Countries.java
@@ -0,0 +1,14 @@
+package com.ddvader44.trackcovid_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Countries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countries);
+    }
+}
--- app/src/main/java/com/ddvader44/trackcovid_19/MainActivity.java
+++ app/src/main/java/com/ddvader44/trackcovid_19/MainActivity.java
@@ -2,6 +2,8 @@ package com.ddvader44.trackcovid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ScrollView;
@@ -17,6 +19,7 @@ import com.android.volley.toolbox.Volley;
 import com.leo.simplearcloader.SimpleArcLoader;
 
 import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -58,14 +61,41 @@ public class MainActivity extends AppCompatActivity {
 
                             cases.setText(jsonObject.getString("cases"));
                             recovered.setText(jsonObject.getString("recovered"));
+                            critical.setText(jsonObject.getString("critical"));
+                            active.setText(jsonObject.getString("active"));
+                            todayCases.setText(jsonObject.getString("todayCases"));
+                            totalDeaths.setText(jsonObject.getString("deaths"));
+                            todayDeaths.setText(jsonObject.getString("todayDeaths"));
+                            affectedCountries.setText(jsonObject.getString("affectedCountries"));
+
+                            pieChart.addPieSlice(new PieModel("Cases",Integer.parseInt(cases.getText().toString()), Color.parseColor("#FFA726")));
+                            pieChart.addPieSlice(new PieModel("Recovered",Integer.parseInt(recovered.getText().toString()), Color.parseColor("#66BB6A")));
+                            pieChart.addPieSlice(new PieModel("Deaths",Integer.parseInt(totalDeaths.getText().toString()), Color.parseColor("#EF5350")));
+                            pieChart.addPieSlice(new PieModel("Active",Integer.parseInt(active.getText().toString()), Color.parseColor("#29B6F6")));
+                            pieChart.startAnimation();
+
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
+
+
+
 
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                            scrollView.setVisibility(View.VISIBLE);
+
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                scrollView.setVisibility(View.VISIBLE);
+
                 Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
@@ -74,6 +104,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void trackYourCountry(View view) {
-
+    startActivity(new Intent(MainActivity.this,Countries.class));
     }
 }
